fe139641d0b9ed4d45a5eb63d8fe4550c0a81fb9
roopeshkumar11 <roopeshkumarbxr2017@gmail.com>
2026-02-12 21:53:05 +0530

    update'

--- sync.js
+++ sync.js
@@ -1,8 +1,8 @@
-import sequelize from "./src/config/db.js";
-import User from "./src/models/User.models.js";
-import Task from "./src/models/Task.models.js";
-import ActivityLog from "./src/models/ActivityLog.models.js";
-await sequelize.sync({ alter: true });
+// import sequelize from "./src/config/db.js";
+// import User from "./src/models/User.models.js";
+// import Task from "./src/models/Task.models.js";
+// import ActivityLog from "./src/models/ActivityLog.models.js";
+// await sequelize.sync({ alter: true });
 
-console.log("Databse created successfully");
-process.exit();
+// console.log("Databse created successfully");
+// process.exit();
