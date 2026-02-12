0bd21b4b7e897ac2e48c629e0f4240832476c9e0
roopeshkumar11 <roopeshkumarbxr2017@gmail.com>
2026-02-13 01:07:04 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,29 +1,57 @@
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     logging: false,
+//   }
+// );
+
+// const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("MySQL Connected");
+//   } catch (error) {
+//     console.error("DB not connecct Error:", error);
+//   }
+// };
+
+// connectDB();
+
+// export default sequelize;
+// 0011roop@123
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("MySQL Connected");
+    console.log("Supabase PostgreSQL Connected ✅");
   } catch (error) {
-    console.error("DB not connecct Error:", error);
+    console.error("DB Connection Error ❌:", error);
   }
 };
 
 connectDB();
-
 export default sequelize;
-
